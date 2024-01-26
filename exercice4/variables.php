c42416ff5256b04e4fbb8a77bae359223c81b729
Harlequelrah <degbovimax19@gmail.com>
2024-01-26 08:33:16 +0000

    exercice 1 à 8 en php

--- exercice4/variables.php
+++
@@ -1,4 +0,0 @@
-<?php
-$languages = array("Allemand", "Anglais", "Français", "Portugais", "Italien", "Espagnol", "Japonais", "Chinois");
-$competences = array("XHTML", "PHP", "MYSQL", "ASP.NET", "JAVA");
-?>
